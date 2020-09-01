a1e5fb90d7f13cf16842073a062cbbfaf326ec77
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-09-01 11:47:18 +0530

    renamed toggleTitleView to toggleEditablity

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,29 +6,25 @@ class Header extends React.Component {
     super(props);
     this.state = { isEditable: false };
     this.saveNewTitle = this.saveNewTitle.bind(this);
-    this.toggleTitleView = this.toggleTitleView.bind(this);
+    this.toggleEditability = this.toggleEditability.bind(this);
   }
 
-  toggleTitleView() {
+  toggleEditability() {
     this.setState(state => ({ isEditable: !state.isEditable }));
   }
 
   saveNewTitle(newTitle) {
     this.props.updateTitle(newTitle);
-    this.toggleTitleView();
+    this.toggleEditability();
   }
 
   render() {
     const defaultHeader = (
-      <div onClick={this.toggleTitleView}>{this.props.value}</div>
+      <div onClick={this.toggleEditability}>{this.props.value}</div>
     );
-    
+
     const editableHeader = (
-      <InputBar
-        className='header'
-        value={this.props.value}
-        onEnter={this.saveNewTitle}
-      />
+      <InputBar value={this.props.value} onEnter={this.saveNewTitle} />
     );
 
     return (
